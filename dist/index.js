54c79fedd429c47ce9642bfdae0cf7eeac2ae801
abdul rehman <khanzadaaabdulrehman222gmail.com>
2023-05-05 00:14:55 +0500

    create a new branch

--- dist/index.js
+++ dist/index.js
@@ -8,31 +8,4 @@ let sales = 123456789;
 let course = 'TypeScript';
 let is_published = true;
 let level;
-let data = ["Shameel", "Usama", "AbdulRehman"];
-console.log(data);
-let user = ["Ali", 2, true];
-console.log(user);
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-;
-let mySize = Size.Small;
-console.log(mySize);
-function calculateTax(income) {
-    if (income < 5000)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculateTax(1000);
-let employee = {
-    id: 1,
-    name: 'Shameel',
-    retire: (date) => {
-        console.log(date);
-    }
-};
-console.log(employee);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -12,38 +12,3 @@ let level;
 //     console.log(document)
 // }
 
-// array
-let data: string[] = ["Shameel", "Usama", "AbdulRehman"]
-console.log(data)
-
-// tuples
-let user: [string, number, boolean] = ["Ali", 2, true]
-console.log(user);
-
-// enum
-enum Size { Small = 1, Medium, Large };
-let mySize: Size = Size.Small;
-console.log(mySize);
-
-// function
-function calculateTax(income: number): number {
-    if (income < 5000)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculateTax(1000);
-
-// object with function and aliases 
-type Employee = {
-    id: number,
-    name: string,
-    retire: (date: Date) => void
-}
-let employee: Employee = {
-    id: 1,
-    name: 'Shameel',
-    retire: (date: Date) => {
-        console.log(date)
-    }
-};
-console.log(employee);
