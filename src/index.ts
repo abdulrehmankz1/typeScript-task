230c74897c933a6142f62d2abb0f0e8bcf1829c1
abdul rehman <khanzadaaabdulrehman222gmail.com>
2023-05-05 00:06:58 +0500

    create a typescript project and start basic typescript practice

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+let age: number = 20;
+console.log("hello")
+if (age < 50)
+    age += 10;
+console.log(age)
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+
+// function render(document){
+//     console.log(document)
+// }
+
+// array
+let data: string[] = ["Shameel", "Usama", "AbdulRehman"]
+console.log(data)
+
+// tuples
+let user: [string, number, boolean] = ["Ali", 2, true]
+console.log(user);
+
+// enum
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Small;
+console.log(mySize);
+
+// function
+function calculateTax(income: number): number {
+    if (income < 5000)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(1000);
+
+// object with function and aliases 
+type Employee = {
+    id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+let employee: Employee = {
+    id: 1,
+    name: 'Shameel',
+    retire: (date: Date) => {
+        console.log(date)
+    }
+};
+console.log(employee);
