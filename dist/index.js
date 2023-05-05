429661219c9483bd7c0786b020a940d8dbbb2f38
abdul rehman <khanzadaaabdulrehman222gmail.com>
2023-05-05 16:31:26 +0500

    try union data type with string and boolean

--- dist/index.js
+++ dist/index.js
@@ -8,4 +8,37 @@ let sales = 123456789;
 let course = 'TypeScript';
 let is_published = true;
 let level;
+let studentsData = ["Shameel", "Usama", "AbdulRehman"];
+console.log(studentsData);
+let user = ["Ali", 2, true];
+console.log(user);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Small;
+console.log(mySize);
+function calculateTax(income) {
+    if (income < 5000)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(1000);
+let employee = {
+    id: 1,
+    name: 'Shameel',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+console.log(employee);
+let data = "AbdulRehman";
+data = 2023;
+data = true;
+console.log(data);
+let dataType = "string";
+dataType = false;
 //# sourceMappingURL=index.js.map
